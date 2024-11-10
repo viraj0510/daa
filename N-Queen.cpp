2ebcf038057fa154abd5120a4da7619268a91b25
Atmaram Korgaonkar <120163458+viraj0510@users.noreply.github.com>
2024-11-10 21:48:29 +0530

    Update N-Queen.cpp

--- N-Queen.cpp
+++ N-Queen.cpp
@@ -1,95 +1,49 @@
 #include <iostream>
-
-#include <cmath> // for abs
+#include <cmath> 
 
 using namespace std;
-
 int board[20], count;
-
 void print(int n) {
-
  cout << "\n\nSolution " << ++count << ":\n\n";
-
- // Print the column numbers
-
  for (int i = 1; i <= n; ++i)
-
  cout << "\t" << i;
-
- // Print the chessboard with queens and empty spaces
-
  for (int i = 1; i <= n; ++i) {
-
- cout << "\n\n" << i; // Print the row number
-
+ cout << "\n\n" << i; 
  for (int j = 1; j <= n; ++j) {
-
  if (board[i] == j) {
-
- cout << "\tQ"; // Print 'Q' for a queen
-
+ cout << "\tQ"; 
  } else {
-
- cout << "\t-"; // Print '-' for empty space
-
+ cout << "\t-"; 
  }
-
  }
-
  }
-
 }
 
 bool place(int row, int column) {
-
  for (int i = 1; i < row; ++i) {
-
  if (board[i] == column || abs(board[i] - column) == abs(i - row))
-
- return false; // There is a conflict
-
+ return false; 
  }
-
- return true; // No conflict
-
+ return true; 
 }
-
 void queen(int row, int n) {
-
  for (int column = 1; column <= n; ++column) {
-
  if (place(row, column)) {
-
- board[row] = column; // Place queen
-
+ board[row] = column; 
  if (row == n) {
-
- print(n); // Print the solution
-
+ print(n); 
  } else {
-
- queen(row + 1, n); // Place the next queen
-
+ queen(row + 1, n); 
  }
-
  }
-
  }
-
 }
 
 int main() {
-
  int n;
-
  cout << " - N Queens Problem Using Backtracking -";
-
  cout << "\n\nEnter number of Queens: ";
-
  cin >> n;
-
  queen(1, n);
-
  return 0;
-
 }
